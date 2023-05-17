9e26eea469ecff91ef1d52f48705b5c166470af3
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2023-05-16 18:27:45 -0700

    Add shell scripts

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -d "bin" src/receiver/Receiver.java src/sender/Sender.java src/shared/AESCipher.java src/shared/MACAlgorithm.java src/shared/RSACipher.java
---
+++ sender.sh
@@ -0,0 +1,1 @@
+java -cp "bin" sender.Sender
